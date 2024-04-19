de53718419879d2907f0d8e6c59edb9b8967a895
sskkzk <161902539+sskkzk@users.noreply.github.com>
2024-04-19 12:16:45 +0000

    16章終了

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   get 'homes/about', to: 'homes#about', as: 'about'
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
 end
