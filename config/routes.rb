e4793f48ddaa743d73e62e9bccab27be98fdf2bc
sskkzk <161902539+sskkzk@users.noreply.github.com>
2024-04-20 06:19:43 +0000

    19章終了

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get 'homes/about', to: 'homes#about', as: 'about'
   
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    # 今まではresourcesでしたが、ここでは、resourceとなっている点に注目してください。 単数形にすると、/:idがURLに含まれなくなります。
+    resource :favorite, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
